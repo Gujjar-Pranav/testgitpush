81660ec3543c6d9faed3a465f54a2c7d552387bc
Gujjar-Pranav <mr.pranavgujjar@gmail.com>
2024-01-01 12:26:31 +0000

    This is my first mongodb connect with pycharm code as well github push

---
+++ mongotest1.py
@@ -0,0 +1,21 @@
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://Pranav:Dhruvi24@cluster0.sr0ekjw.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+db = client.test
+print(db)
+
+d = {
+    "name": "Pranav",
+    "surname": "Gujjar",
+    "email_id": "pranav1329@gmail.com"
+}
+
+d4 = {"car":"hyndai","bike":"yamaha","plane":"vistara"}
+
+mdb = client['mongodbsetup_1']
+coll = mdb['test']
+coll.insert_one(d)
+coll.insert_one(d4)
